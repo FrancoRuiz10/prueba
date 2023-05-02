346d104e036743087be0eefb0f82a14eda7322eb
Franco Ruiz <francojruiz10@gmail.com>
2023-05-02 20:33:14 -0300

    Primera clase de javascript

---
+++ Diseño 11/main.js
@@ -0,0 +1,8 @@
+let minombre = "Franco"
+let miapellido = "Ruiz"
+let miedad=23
+let mimascota= "Mora"
+const espacio= " "
+
+let nombre= prompt ("Ingrese su nombre")
+let edad= prompt (nombre+espacio+"Ingrese su edad")
