664baffc3e887ef56b16318353a275d8cb7eafbc
Franco Ruiz <francojruiz10@gmail.com>
2023-05-05 12:48:25 -0300

    tarea sumas de js

---
+++ Diseño 13/main.js
@@ -0,0 +1,4 @@
+var numero1=parseInt(prompt("Ingrese un numero"))
+var numero2=parseInt(prompt("Ingrese orto numero")) //parseint se utiliza a la hora de poner numeros 
+var numero3=numero1+numero2
+alert("el resultado es: "+numero3)
