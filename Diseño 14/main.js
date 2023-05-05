db7768405889a398a2c830f7bbb234512b7154ee
Franco Ruiz <francojruiz10@gmail.com>
2023-05-05 13:04:09 -0300

    tarea con if

---
+++ Diseño 14/main.js
@@ -0,0 +1,2 @@
+let numero = prompt("Ingrese un numero")
+let mayor = (numero > 9) ? alert("Su numero es mayor a 9") : (numero < 9) ? alert("Su numero es menor a 9") : alert("Usted no ingreso ningun numero")
