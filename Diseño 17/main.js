b315047a4c617caaf6194693d66e4f72f567de6a
Franco Ruiz <francojruiz10@gmail.com>
2023-05-11 20:51:59 -0300

    tarea while if function y mas

--- Diseño 17/main.js
+++ Diseño 17/main.js
@@ -13,13 +13,19 @@ function saludar(nombre, apellido ,serie){
     alert("Hola " +nombre+ " "+apellido+ " Tu serie favorita es  "+serie)
 }
 saludar("Franco", "Ruiz", "Bersek")
-function longitudpalabra(nombre){
-    return nombre.length;
+
+
+console.log(document.querySelector("h1"));
+
+function saludar(){
+    alert("Hola a todos");
 }
 
-
-let longitudnombre=longitudpalabra("Franco");
-console.log(longitudnombre);
-
-
-
+let nombre=prompt("Ingrese su nombre y apellido");
+let cursos=prompt("Ingrese el numero de curso");
+while (cursos != 58){
+    alert("curso equivocado")
+    cursos=prompt("Ingrese su curso nuevamente: ")
+}if(cursos==58){
+    alert(nombre+" Ingreso al curso "+cursos)
+}
